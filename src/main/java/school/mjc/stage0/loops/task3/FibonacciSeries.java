b8337cd9218ae2e43aa6b8eb277393902b83eaf9
dilyor91 <dilyor_05@mail.ru>
2022-07-30 11:35:54 +0500

    Task #3. For Loops

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -1,6 +1,22 @@
 package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
-    public void printFibonacci(int lastFibonacci) {
+    public  void printFibonacci(int lastFibonacci) {
+        int cnt = 1, fib, fib_1=0, fib_2=0;
+        while(cnt <= lastFibonacci) {
+            if(cnt==1) {
+                fib_1 = 0;
+                System.out.println(0);
+            } else if(cnt==2) {
+                fib_2 = 1;
+                System.out.println(1);
+            } else {
+                fib = fib_1+fib_2;
+                System.out.println(fib);
+                fib_1 = fib_2;
+                fib_2 = fib;
+            }
+            cnt++;
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -1,6 +1,22 @@
 package school.mjc.stage0.loops.task3;
 
 public class GreatestCommonDivisor {
-    public void printGCD(int first, int second) {
+    public  void printGCD(int first, int second) {
+
+        if(first==0) {
+            System.out.println(second);
+        } else if(second == 0) {
+            System.out.println(second);
+        } else {
+        while(first!=second) {
+            if(first>second) {
+                first = first - second;
+            } else {
+                second = second - first;
+            }
+        }
+           System.out.println(first);
+        }
     }
 }
+
