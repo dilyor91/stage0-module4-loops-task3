358222631633a1affdbc5e90ed1a8aa31004471c
dilyor91 <dilyor_05@mail.ru>
2022-08-02 12:59:17 +0500

    Task #3. For Loops v2

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-
+        if(lengthOfLastNumber<=0)
+            System.out.println(0);
+        else {
+            int sum = 0, p = 0;
+            for(int i=1;i<=lengthOfLastNumber;i++){
+                p = p*10+9;
+                sum+=p;
+            }
+            System.out.println(sum);
+        }
     }
 }
